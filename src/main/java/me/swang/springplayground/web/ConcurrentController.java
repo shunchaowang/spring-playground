816fb29570277e4905b77fa6bd77fd8276863113
Shunchao Wang <ironaire@gmail.com>
2019-09-15 17:49:40 -0400

    working on callable async.

--- src/main/java/me/swang/springplayground/web/ConcurrentController.java
+++ src/main/java/me/swang/springplayground/web/ConcurrentController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @RestController
 @RequestMapping("/concurrent")
 public class ConcurrentController {
@@ -13,10 +15,28 @@ public class ConcurrentController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/sync")
-    public String getValueSync() {
+    public String helloWorldSync() {
         logger.info("Request received");
-        return processRequest();
+        String sync = processRequest();
+        logger.info("Servlet thread released");
+        return sync;
     }
+
+    @GetMapping("/callable")
+    public Callable<String> helloWorldCallable() {
+        logger.info("Request received");
+//        Callable<String> callable = this::processRequest;
+//        Callable<String> callable = new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                return processRequest();
+//            }
+//        };
+        Callable<String> callable = () -> processRequest(); // lambda
+        logger.info("Servlet thread released");
+        return callable;
+    }
+
 
     private String processRequest() {
         logger.info("Start processing request");
